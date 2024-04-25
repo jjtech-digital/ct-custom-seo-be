e436cf6ad8562a2bdd8ba0286cc1ddcec4cc8b26
Shubham-compose-tech <shubham@compose.co.in>
2024-04-25 17:04:17 +0530

    fix: cors issue

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   //   methods: 'GET, POST, PUT, DELETE',
   //   allowedHeaders: 'Content-Type, Authorization',
   // });
-  // app.enableCors();
+  app.enableCors();
 
   // app.use((req, res, next) => {
   //   res.header('Access-Control-Allow-Origin', '*');
@@ -25,11 +25,11 @@ async function bootstrap() {
   //   next();
   // });
 
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-    credentials: true
-  });
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: '*',
+  //   credentials: true
+  // });
 
   await app.listen(PORT);
   console.log(`Started server listening on : ${await app.getUrl()}`);
