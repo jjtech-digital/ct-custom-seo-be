8256febc0dd6bce075d9881ef44bb2957b7f5cf7
Shubham-compose-tech <shubham@compose.co.in>
2024-04-25 13:45:35 +0530

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3002;
   
   // cors setup
-  app.enableCors({
-    origin: ['http://localhost:3002', 'https://ct-custom-seo-be.vercel.app/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  })
+  app.enableCors()
   await app.listen(PORT);
   console.log(`Started server listeing on : ${await app.getUrl()}`);
 }
